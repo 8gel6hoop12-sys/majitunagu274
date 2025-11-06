5f578de4a8ca7b89bfbcdc9e31959f63a29fd13e
8gel6hoop12-sys <8gel6hoop12@gmail.com>
2025-11-06 20:07:23 +0900

    Replace StrictMode with BrowserRouter in main.tsx

--- main.tsx
+++ main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
-import './i18n'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+// src/main.tsx（例）
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter basename={__BASE_PATH__ as string}>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+)
 )
